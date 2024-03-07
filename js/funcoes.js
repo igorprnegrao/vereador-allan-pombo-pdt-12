6d7ef7bbb72c57673eca3dcf6659a8dad23452b8
Negrão, IPR <neurocientista@gmail.com>
2024-03-07 14:25:23 -0300

    .

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+let btnMenu = document.getElementById('btn-abrir-menu')
+
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=> {
+    menu.classList.add('abrir-menu')
+} )
