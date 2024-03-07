907ebc2f4131a3b466442d9d0eb84c8417ae484b
Negrão, IPR <neurocientista@gmail.com>
2024-03-07 15:44:02 -0300

    menu mobile final

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,6 +2,8 @@ let btnMenu = document.getElementById('btn-abrir-menu')
 
 let menu = document.getElementById('menu-mobile')
 
+let overlay = document.getElementById('overlay-menu')
+
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
 });
